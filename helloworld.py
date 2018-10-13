6069893b1db4c87588cdb6ad1aea05b81222218b
yang6020 <justinvyang@gmail.com>
2018-10-12 18:25:15 -0700

    add functions, comments and other practices

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,42 @@
 #!/usr/local/bin/python3
+
+# pylint: disable=E0401
 import requests
 
 response = requests.get('https://httpbin.org/ip')
+# single line comment
+""" 
+multi 
+line
+comment 
+"""
+
+isTrue = True
+
+
+def printme(varIsTrue):
+    if varIsTrue:
+        print('true')
+    else:
+        print('false')
+
+
+longname = \
+    "hello" + \
+    "long" + \
+    "world"
+
+num1 = 1
+num3 = num1
+
+num4 = num3 + num1
+
 
 print('Your IP is {0}'.format(response.json()['origin']))
-print('hello world')
+print(num4)
+num1 = 2
+print(num4)
+print(longname)
+printme(isTrue)
+isTrue = False
+printme(isTrue)
