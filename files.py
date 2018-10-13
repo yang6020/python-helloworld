419c49bf53fc02122733f8f0117b0dcb135bb0a9
yang6020 <justinvyang@gmail.com>
2018-10-13 01:48:07 -0700

    add file reading and writing in files.py using animals.txt

---
+++ files.py
@@ -0,0 +1,22 @@
+# open/open modes
+animals = open('animals.txt', 'r+')
+# r  = open for read (default)
+# w  = open for write, truncate
+# r+ = open for read/write
+# w+ = open for read/write, truncate
+# a+ = open for read/append
+
+# read
+animals.seek(0)
+text = animals.read()
+print(text)
+
+# write
+animals.write('dog\n')
+animals.write('elephant\n')
+animals.seek(0)
+text2 = animals.read()
+print(text2)
+
+# close
+animals.close()
